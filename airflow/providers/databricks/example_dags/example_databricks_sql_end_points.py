9ea4b311b7ef3736c7fecf50010f9564e5c96ddc
Senthilkumar Balasubramanian <sbalasubramanian@atlassian.com>
2022-02-19 17:16:14 +0530

    Added Taskid

--- airflow/providers/databricks/example_dags/example_databricks_sql_end_points.py
+++ airflow/providers/databricks/example_dags/example_databricks_sql_end_points.py
@@ -34,7 +34,8 @@ https://docs.databricks.com/api/latest/jobs.html#runstate
 from datetime import datetime
 
 from airflow import DAG
-from airflow.providers.databricks.operators.databricks import DatabricksGetSqlEndpointsOperator
+from airflow.providers.databricks.operators.databricks import DatabricksGetSqlEndpointsOperator, \
+    DatabricksCreateSqlEndpointOperator
 
 with DAG(
     dag_id='example_databricks_endpoint_operator',
@@ -45,5 +46,22 @@ with DAG(
 ) as dag:
     import os
     # databricks_connection = os.environ["AIRFLOW_CONN_DATABRICKS_DEFAULT"]
-    task = DatabricksGetSqlEndpointsOperator(task_id='get_sql_end_points',)
-    print(task.execute())
+    task1 = DatabricksGetSqlEndpointsOperator(task_id='get_sql_end_points',)
+    #print(task1.execute())
+
+    json = {
+        "name": "New SQL Endpoint",
+        "cluster_size" : "2X-Small",
+        "min_num_clusters" : 1,
+        "max_num_clusters": 1,
+        "auto_stop_mins" : 10,
+        "tags" : [],
+        "spot_instance_policy": "COST_OPTIMIZED",
+        "enable_photon" : False,
+        "enable_serverless_compute": False,
+        "channel" : "CHANNEL_NAME_CURRENT"
+    }
+
+    task2 = DatabricksCreateSqlEndpointOperator(task_id='create_sql_end_points', json)
+    #print(task2.execute())
+    task1 >> task2
